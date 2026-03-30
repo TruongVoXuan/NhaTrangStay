203fa6330796f14fd9d50cbcdfdaa5c1ee69cb64
TruongVoXuan <truong.75.cvan@gmail.com>
2026-03-30 10:17:50 +0700

    init backend in folder

---
+++ nhatrangstay/src/test/java/com/truongvx64cntt/nhatrangstay/NhatrangstayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truongvx64cntt.nhatrangstay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NhatrangstayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
