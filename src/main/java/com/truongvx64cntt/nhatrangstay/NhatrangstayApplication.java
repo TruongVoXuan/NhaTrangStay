a40a7156e36c50baa148cd7d17b073bdbe993b67
TruongVoXuan <truong.75.cvan@gmail.com>
2026-03-30 10:12:09 +0700

    init backend spring boot

---
+++ src/main/java/com/truongvx64cntt/nhatrangstay/NhatrangstayApplication.java
@@ -0,0 +1,13 @@
+package com.truongvx64cntt.nhatrangstay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NhatrangstayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NhatrangstayApplication.class, args);
+	}
+
+}
